451a27f720d3e960aa874e18a7a3c530afe52273
ritul-alt <ritanand@deloitte.com>
2023-09-25 14:56:56 +0530

    added logo

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,21 +1,18 @@
 import React from "react";
-// import logo from "../../assets/images/pfizer_logo.png";
+import logo from "../assets/images/pfizer_logo.png";
 import "./Header.css";
 
 function Header() {
   //   const navigate = useNavigate();
   return (
     <div>
-      {/* <img
+      <img
         src={logo}
         className="pfizer-logo"
         // onClick={() => navigate("/template")}
         href="/home"
-      /> */}
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <span className="wrdm">
-        <b>WRDM</b>
-      </span>
+      />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chat{" "}
     </div>
   );
 }
